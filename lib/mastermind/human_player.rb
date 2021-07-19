f843f44ab5ca5b6450cfd1351b7fd47486e48446
PlaustralCL <plaustralcl@gmail.com>
2021-07-19 19:15:36 -0400

    Refactor instance variables in HumanPlayer

--- lib/mastermind/human_player.rb
+++ lib/mastermind/human_player.rb
@@ -5,24 +5,30 @@
 module MasterMind
   # Behavior and state for the human player: guesses or codes
   class HumanPlayer
-    attr_accessor :guess
+    attr_reader :guess
+
+    def initialize
+      @guess = ""
+    end
 
     # @return [Array]
     def make_guess
       receive_guess
-      @guess.to_i.digits.reverse
+      # convert guess to an array
+      guess.to_i.digits.reverse
     end
 
     def valid_guess?
-      true if @guess =~ /^[1-6]{4}$/
+      true if guess =~ /^[1-6]{4}$/
     end
 
     # Requests guess until a valid guess is received
+    # @return [String] @guess
     def receive_guess
       loop do
         print "Please enter your guess: "
         @guess = gets.chomp
-        return @guess if valid_guess?
+        return guess if valid_guess?
 
         puts "That was not a valid guess. Please try again."
       end
