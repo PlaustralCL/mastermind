33679bef9e0912072aa55ae621b12804f04214c1
PlaustralCL <plaustralcl@gmail.com>
2021-07-29 07:31:53 -0400

    Clean up file

--- lib/mastermind.rb
+++ lib/mastermind.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 
-
 require_relative "./mastermind/clues"
 require_relative "./mastermind/computer_player"
 require_relative "./mastermind/game"
@@ -8,24 +7,6 @@ require_relative "./mastermind/human_player"
 require_relative "./mastermind/secret_code"
 require_relative "./mastermind/set_up"
 
+# Module to organize all the classes needed for the master mind game
 module MasterMind
-
-  def self.new_game
-    welcome
-  end
-
-  def self.welcome
-    intro = <<~HEREDOC
-      Welcome to Master Mind!
-      You will be the code breaker, breaking the code created by the computer
-      After each guess you will be provided clues:
-      X = Right number in the right position
-      O = Right number in the wrong position
-      You have 12 attempts to break the code.
-    HEREDOC
-
-    puts intro
-    puts ""
-  end
-
 end
