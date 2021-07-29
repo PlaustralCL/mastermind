a3a59fec1041e3e62a557200e94332456cad70d1
PlaustralCL <plaustralcl@gmail.com>
2021-07-29 07:19:54 -0400

    Add comments

--- lib/mastermind/secret_code.rb
+++ lib/mastermind/secret_code.rb
@@ -1,18 +1,20 @@
 # frozen_string_literal: true
 
 module MasterMind
-
   # Generates the secret code, stored as an array
   class SecretCode
     attr_reader :value
 
-    # @param [Array] value
+    # @param [Array] value. Allows an array to be passed upon initialization if
+    # a specific code is needed, such as for troubleshooting or testing.
     def initialize(value = random_code)
       @value = value
     end
 
     private
 
+    # Generates a 4 digit code using the numbers 1 - 6. Each digit of the code
+    # is generated separately. Repeated numbers are allowed, but no blank spaces.
     # @return [Array]
     def random_code
       code_length = 4
