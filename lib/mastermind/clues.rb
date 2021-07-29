5eb0c8556311730ba06157043cf751cb244cf7e2
PlaustralCL <plaustralcl@gmail.com>
2021-07-29 06:40:10 -0400

    Refactor for Rubocop
    Address Rubocop comments. Add more explanatory comments.

--- lib/mastermind/clues.rb
+++ lib/mastermind/clues.rb
@@ -18,7 +18,7 @@ module MasterMind
     def initialize(guess:, code:)
       @guess = [].replace(guess)
       @code = [].replace(code)
-      @key = Array.new
+      @key = []
     end
 
     # @return [String] - An alphabetized string with the clues
@@ -30,31 +30,40 @@ module MasterMind
 
     private
 
+    # Compares each element of the code with the corresponding element of the guess.
+    # If a match is found the key array is updated, the code element is replaced with 0
+    # and the guess element is replaced with 99. The code and guess element replacements
+    # are different to avoid false positives when moving on to the near match comparison.
     def exact_match
       code.each_index do |index|
-        if code[index] == guess[index]
-          key.push("X")
-          code[index] = 0
-          guess[index] = 99
-        end
+        next unless code[index] == guess[index]
+
+        key.push("X")
+        code[index] = 0
+        guess[index] = 99
       end
     end
 
+    # Cycle through the guess. This will cycle through the code and the guess
     def near_match
       (0..3).each do |guess_index|
         compare_for_near_match(guess_index)
       end
     end
 
+    # Cycle through the code, comparing it to the guess. Each digit of guess can
+    # have only one clue, so the loop breaks as soon as a clue is added to the key array.
+    # The code digit (code[code_index]) is replaced with 0 to keep it from being used again.
+    # Without this, extra keys get generated
+    # @param [Integer] guess_index - index of the guess array to use for comparision to the code
     def compare_for_near_match(guess_index)
       (0..3).each do |code_index|
-        if guess[guess_index] == code[code_index]
-          key.push("O")
-          code[code_index] = 0
-          break
-        end
+        next unless guess[guess_index] == code[code_index]
+
+        key.push("O")
+        code[code_index] = 0
+        break
       end
     end
-
   end
 end
